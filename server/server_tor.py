78025fa9e60a479d230e0a963c66f9a3d0ce9595
Baccount <cassias.long0r@icloud.com>
2023-02-21 11:40:47 -0800

    tor launching works, kill tor needs work

--- server/server_tor.py
+++ server/server_tor.py
@@ -1,18 +1,55 @@
 import socket
 from random import randint
 from stem.control import Controller
-import os
+from stem import process
+import os, signal
+from time import sleep
+import subprocess
 
 class CreateOnion():
     
     def __init__(self):
+        # stop tor
+        self.stop_tor()
+        # start tor
+        self.start_tor()
+        sleep(2)
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.port = randint(10000, 65535)
-        self.controller = Controller.from_port(port = 9051)
+        self.controller = Controller.from_port(port=9051)
         self.controller.authenticate()
         self.socket.bind(("127.0.0.1", self.port))
         self.socket.listen(10)
+    def stop_tor(self):
+        # iterating through each instance of the process
+        try:
+            name = "tor"
+            for line in os.popen("ps ax | grep " + name + " | grep -v grep"):
+                fields = line.split()
+                # extracting Process ID from the output
+                pid = fields[0]
+                # terminating process
+                os.kill(int(pid), signal.SIGKILL)
+            print("Process Successfully terminated")
+        except Exception as e:
+            print(e)
 
+    def start_tor(self):
+        # path to the tor binary
+        tor_dir = os.getcwd() + '/tor_bin/tor'
+        # create a new Tor configuration
+        tor_cfg = {
+            'SocksPort': '9050',
+            'ControlPort': '9051',
+            'CookieAuthentication': '1',
+        }
+
+        # start Tor with the new configuration
+        self.tor_procc = process.launch_tor_with_config(
+            config=tor_cfg,
+            tor_cmd=tor_dir,  # path to your tor binary
+            timeout=60
+        )
 
     def ephemeral_onion(self):
         '''
--- server_start.py
+++ server_start.py
@@ -1,39 +1,39 @@
 from server.server import Server
-import os
-import subprocess
-from time import sleep
-import psutil
+# import os
+# import subprocess
+# from time import sleep
+# import psutil
 
-# check if brew is installed and install if not on mac
-try:
-    subprocess.check_output(['brew', '--version'])
-except subprocess.CalledProcessError:
-    print("Brew is not installed, installing now...")
-    os.system("/bin/bash -c \"$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)\"")
+# # check if brew is installed and install if not on mac
+# try:
+#     subprocess.check_output(['brew', '--version'])
+# except subprocess.CalledProcessError:
+#     print("Brew is not installed, installing now...")
+#     os.system("/bin/bash -c \"$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)\"")
 
 
-try:
-    subprocess.check_output(['which', 'tor'])
-except subprocess.CalledProcessError:
-    print('Tor is not installed')
-    os.system("brew install tor")
+# try:
+#     subprocess.check_output(['which', 'tor'])
+# except subprocess.CalledProcessError:
+#     print('Tor is not installed')
+#     os.system("brew install tor")
 
-# check if tor is running and start if not
+# # check if tor is running and start if not
 
-try:
-    subprocess.check_output(['which', 'tor'])
-except subprocess.CalledProcessError:
-    print('Tor is not installed')
-    os.system("brew install tor")
+# try:
+#     subprocess.check_output(['which', 'tor'])
+# except subprocess.CalledProcessError:
+#     print('Tor is not installed')
+#     os.system("brew install tor")
 
-# Check if Tor is running
-for proc in psutil.process_iter():
-    if proc.name() == 'tor':
-        print('Tor is running')
-        break
-else:
-    print('Tor is not running')
-    os.system("brew services restart tor")
-    sleep(5)
+# # Check if Tor is running
+# for proc in psutil.process_iter():
+#     if proc.name() == 'tor':
+#         print('Tor is running')
+#         break
+# else:
+#     print('Tor is not running')
+#     os.system("brew services restart tor")
+#     sleep(5)
 server = Server()
 server.start()
