499c3ddd9d70ff04a07485650b5096bfa78a5af1
Baccount <cassias.long0r@icloud.com>
2023-02-21 17:03:17 -0800

    first working build!

--- server/server_tor.py
+++ server/server_tor.py
@@ -1,61 +1,38 @@
 import socket
 from random import randint
 from stem.control import Controller
-from stem import process
-import os, signal
-from time import sleep
-import subprocess
+import os
+
+from onionshare.onion import Onion
+from onionshare.common import Common
+from onionshare.settings import Settings
+
+
 
 class CreateOnion():
-    
+
     def __init__(self):
-        # stop tor
-        self.stop_tor()
-        # start tor
-        self.start_tor()
-        sleep(2)
+        # TODO NEW CODE BELOW
+        self.common = Common(verbose=True)
+        self.onion = Onion(self.common)
+        self.custom_settings = Settings(self.common)
+        self.custom_settings.set("connection_type", "bundled")
+        self.onion.connect(custom_settings=self.custom_settings)
+
+        self.socket_port = randint(10000, 65535)
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.port = randint(10000, 65535)
-        self.controller = Controller.from_port(port=9051)
+        self.port = self.onion.get_control_port()
+        self.controller = Controller.from_port(port=self.port)
         self.controller.authenticate()
-        self.socket.bind(("127.0.0.1", self.port))
+        self.socket.bind(("127.0.0.1", self.socket_port))
         self.socket.listen(10)
-    def stop_tor(self):
-        # iterating through each instance of the process
-        try:
-            name = "tor"
-            for line in os.popen("ps ax | grep " + name + " | grep -v grep"):
-                fields = line.split()
-                # extracting Process ID from the output
-                pid = fields[0]
-                # terminating process
-                os.kill(int(pid), signal.SIGKILL)
-            print("Process Successfully terminated")
-        except Exception as e:
-            print(e)
 
-    def start_tor(self):
-        # path to the tor binary
-        tor_dir = os.getcwd() + '/tor_bin/tor'
-        # create a new Tor configuration
-        tor_cfg = {
-            'SocksPort': '9050',
-            'ControlPort': '9051',
-            'CookieAuthentication': '1',
-        }
-
-        # start Tor with the new configuration
-        self.tor_procc = process.launch_tor_with_config(
-            config=tor_cfg,
-            tor_cmd=tor_dir,  # path to your tor binary
-            timeout=60
-        )
 
     def ephemeral_onion(self):
         '''
         create ephemeral hidden services
         '''
-        return self.controller.create_ephemeral_hidden_service({80: self.port}, await_publication = True)
+        return self.controller.create_ephemeral_hidden_service({80: self.socket_port}, await_publication = True)
 
 
     def non_ephemeral_onion(self):
@@ -66,13 +43,13 @@ class CreateOnion():
         key_path = os.path.join(os.path.dirname(__file__), 'private_key')
 
         if not os.path.exists(key_path):
-            response = self.controller.create_ephemeral_hidden_service({80: self.port}, await_publication = True)
+            response = self.controller.create_ephemeral_hidden_service({80: self.socket_port}, await_publication = True)
             with open(key_path, 'w') as key_file:
                 key_file.write('%s:%s' % (response.private_key_type, response.private_key))
             return response
         else:
             with open(key_path) as key_file:
                 key_type, key_content = key_file.read().split(':', 1)
-            response = self.controller.create_ephemeral_hidden_service({80: self.port}, key_type=key_type, key_content=key_content, await_publication = True)
+            response = self.controller.create_ephemeral_hidden_service({80: self.socket_port}, key_type=key_type, key_content=key_content, await_publication = True)
 
         return response
