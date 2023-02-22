c2eee26c4763d4d61af39531fd29fa8b715140ff
Baccount <cassias.long0r@icloud.com>
2023-02-21 23:26:18 -0800

    GeoIPFile files added to tor config

--- client_start.py
+++ client_start.py
@@ -8,15 +8,19 @@ verbose = True
 # path to the tor binary
 tor_dir = os.getcwd() + '/tor/tor'
 obsf4 = os.getcwd() + '/tor/obfs4proxy'
+geo_ip_file = os.getcwd() + '/tor/geoip'
+geo_ipv6_file = os.getcwd() + '/tor/geoip6'
 # create a new Tor configuration
 tor_cfg = {
     'SocksPort': '9050',
     'ControlPort': '9051',
     'CookieAuthentication': '1',
-    'ClientTransportPlugin': 'obfs4 exec {0}'.format(obsf4),
+    'ClientTransportPlugin': f'obfs4 exec {obsf4}',
     'Bridge': 'obfs4 [2a0c:4d80:42:702::1]:27015 C5B7CD6946FF10C5B3E89691A7D3F2C122D2117C cert=TD7PbUO0/0k6xYHMPW3vJxICfkMZNdkRrb63Zhl5j9dW3iRGiCx0A7mPhe5T2EDzQ35+Zw iat-mode=0',
     'AvoidDiskWrites': '1',
     'Log': 'notice stdout',
+    'GeoIPFile': f'{geo_ip_file}',
+    'GeoIPv6File': f'{geo_ipv6_file}',
 }
 class ClientServer():
     def __init__(self):
