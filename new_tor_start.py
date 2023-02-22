dccf56d088502ee961be0ab08e39a1a59dd48276
Baccount <cassias.long0r@icloud.com>
2023-02-21 16:22:40 -0800

    work on new tor start

--- new_tor_start.py
+++ new_tor_start.py
@@ -1,36 +1,15 @@
-from stem.control import Controller
-from stem import process
-import os
+from onionshare_cli.onion import Onion
+from onionshare_cli.common import Common
+
 
 
 def main():
+    common = Common(verbose=True)
+    onion = Onion(common)
 
-    # path to the tor binary
-    tor_dir = os.getcwd() + '/tor_bin/tor'
-    # create a new Tor configuration
-    tor_cfg = {
-        'SocksPort': '9050',
-        'ControlPort': '9051',
-        'CookieAuthentication': '1',
-    }
-
-    # start Tor with the new configuration
-    tor_process = process.launch_tor_with_config(
-        config=tor_cfg,
-        tor_cmd=tor_dir,  # path to your tor binary
-        timeout=60
-    )
-
-    # connect to the Tor control port to confirm the configuration
-    with Controller.from_port(port=9051) as tor_controller:
-        tor_controller.authenticate()  # provide the password if necessary
-        socks_port = tor_controller.get_conf('SocksPort')  # get the value of the SocksPort parameter
-        print(f'Tor is listening on port {socks_port}')  # print the value of the SocksPort parameter
-    tor_process.kill()  # kill the Tor process
+    onion.connect()
 
 
-    def main():
-        pass
 
 if __name__ == "__main__":
     main()
