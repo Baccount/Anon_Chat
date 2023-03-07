f835f96f579e5b97cf16dbe4355ce1911e2c92ac
Baccount <cassias.long0r@icloud.com>
2023-03-07 10:56:06 -0800

    test_check_data works

--- bridges/downloadbridges.py
+++ bridges/downloadbridges.py
@@ -105,14 +105,16 @@ class DownloadBridges:
         return self.checkData()
 
 
-    def checkData(self):
+    def checkData(self, data=None):
         """ Check the data
 
         Returns:
             bool: True if it exists else False
         """
         try:
-            data = self.bridge.json()["data"]
+            if data is None:
+                # we are not testing
+                data = self.bridge.json()["data"]
             if not data:
                 log_msg("DownloadBridges", "checkCaptcha", "Captcha is incorrect")
                 return False
--- tests/test_get_bridges.py
+++ tests/test_get_bridges.py
@@ -6,6 +6,7 @@ from bridges.downloadbridges import DownloadBridges
 class TestDownloadBridges():
     def setup(self):
         self.db = DownloadBridges(protocol="obfs4", test=True)
+        # test connect Meek
         assert self.db.connectMeek() is True
 
     def test_getCaptcha(self):
@@ -16,3 +17,8 @@ class TestDownloadBridges():
 
     def test_cleanup(self):
         assert self.db.cleanup() is True
+
+    def test_check_data(self):
+        # test checkData
+        data = { "data": [ { "version": "0.1.0", "type": "client-transports", "supported": [ "obfs4" ] } ] }
+        assert self.db.checkData(data) is True
