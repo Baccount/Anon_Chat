7ab37b56fc23d8c6a262a0f1c9d45ca1eba54f3e
Baccount <cassias.long0r@icloud.com>
2023-03-07 18:51:54 -0800

    added ascii art

--- client_start.py
+++ client_start.py
@@ -16,6 +16,7 @@ geo_ipv6_file = os.getcwd() + "/tor/geoip6"
 
 class ClientServer:
     def __init__(self):
+        self.show_ascii()
         self.tor_cfg = {
             "SocksPort": "9050",
             "ControlPort": "9051",
@@ -87,6 +88,19 @@ class ClientServer:
             "UseBridges": "1",
             "Bridge": obsf4Bridges,
         }
+    def show_ascii(self):
+        print("""
+           d8888                             .d8888b.  888               888    
+          d88888                            d88P  Y88b 888               888    
+         d88P888                            888    888 888               888    
+        d88P 888 88888b.   .d88b.  88888b.  888        88888b.   8888b.  888888 
+       d88P  888 888 "88b d88""88b 888 "88b 888        888 "88b     "88b 888    
+      d88P   888 888  888 888  888 888  888 888    888 888  888 .d888888 888    
+     d8888888888 888  888 Y88..88P 888  888 Y88b  d88P 888  888 888  888 Y88b.  
+    d88P     888 888  888  "Y88P"  888  888  "Y8888P"  888  888 "Y888888  "Y888 \n""")
+                                                                            
+                                                                            
+                                                                            
 
 
 if __name__ == "__main__":
