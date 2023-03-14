16da2a745c27cf60889021d2abd8c74a2435bc13
Baccount <cassias.long0r@icloud.com>
2023-03-13 22:46:31 -0700

    added the skip Linux does not support it meek bin

--- tests/test_get_bridges.py
+++ tests/test_get_bridges.py
@@ -8,30 +8,24 @@ sys.path.append("../")
 from bridges.downloadbridges import DownloadBridges
 
 
-#@pytest.mark.skipif(
-#    getenv("GITHUB_ACTIONS") == "true", reason="Skipping test on GitHub Actions"
-#)
+@pytest.mark.skipif(
+    getenv("GITHUB_ACTIONS") == "true", reason="Skipping test on GitHub Actions Because meek-client is not supported on GitHub Actions 'Linux does not support it'."
+)
 class TestDownloadBridges:
     def setup(self):
         self.db = DownloadBridges(protocol="obfs4", test=True)
         # test connect Meek
         assert self.db.connectMeek() is True
-    @pytest.mark.skipif(
-    getenv("GITHUB_ACTIONS") == "true", reason="Skipping test on GitHub Actions"
-    )
+
     def test_getCaptcha(self):
         assert self.db.getCaptcha() is True
 
     def test_display_image(self):
         assert self.db.display_image() is True
-    @pytest.mark.skipif(
-    getenv("GITHUB_ACTIONS") == "true", reason="Skipping test on GitHub Actions"
-    )
+
     def test_cleanup(self):
         assert self.db.cleanup() is True
-    @pytest.mark.skipif(
-    getenv("GITHUB_ACTIONS") == "true", reason="Skipping test on GitHub Actions"
-    )
+
     def test_check_data(self):
         # test checkData
         data = {
