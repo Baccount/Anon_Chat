5c259b259c07819c96d47c32423e131b733381fd
Baccount <cassias.long0r@icloud.com>
2023-03-21 20:14:04 -0700

    disabled extra logging making server logs readable

--- server/server.py
+++ server/server.py
@@ -83,8 +83,7 @@ class Server(Cmd):
             objects.append(buffer[start:end])
             start = end
             end = buffer.find("{", start)
-            # Process each JSON object
-            log_msg("separateJson", f"objects: {objects}")
+            #log_msg("separateJson", f"objects: {objects}")
             return objects
 
     def decode_buffer(self, buffer):
@@ -122,7 +121,7 @@ class Server(Cmd):
             try:
                 buffer = b""
                 chunk = connection.recv(1024)
-                log_msg("__user_thread", f"chunk: {chunk}")
+                #log_msg("__user_thread", f"chunk: {chunk}")
 
                 if chunk == b"" and self.__connections[user_id]:
                     # user still connected, disconnect them
@@ -137,7 +136,7 @@ class Server(Cmd):
                     break
 
                 buffer = self.decode_buffer(buffer)
-                log_msg("__user_thread", f"buffer: {buffer}")
+                #log_msg("__user_thread", f"buffer: {buffer}")
 
                 objects = self.separateJson(buffer)
 
