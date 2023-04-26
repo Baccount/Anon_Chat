fab4b524c7e9b7f62d34002ad19590331d81ca7c
Baccount <cassias.long0r@icloud.com>
2023-04-25 23:14:19 -0700

    small fixes the test_server_start

--- tests/test_server_start.py
+++ tests/test_server_start.py
@@ -1,16 +1,15 @@
 import sys
-sys.path.append('../')
+
+sys.path.append("../")
 
 # trunk-ignore(flake8/E402)
 from server_start import StartServer
 
 
-
-class TestServer():
-# run tests for client
+class TestServer:
+    # run tests for client
     def setup_method(self):
         self.server = StartServer(test=True)
 
-
     def test_server_tor(self):
-        assert  self.server.start() is True
+        assert self.server.start() is True
